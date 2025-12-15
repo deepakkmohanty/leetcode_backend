95ace7932c68978282bf01bca79d651113adfb75
Deepak Mohanty <mohantydeepak284@gmail.com>
2025-12-15 17:26:17 +0530

    Added node env

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-const {MONGODB_URI} = require('./server.config')
+const {MONGODB_URI, NODE_ENV} = require('./server.config')
 const connectDB = async()=>{
     try {
-      await mongoose.connect(MONGODB_URI,{
-        dbName:'Problem',
-      })
+        if(NODE_ENV==='development'){
+            await mongoose.connect(MONGODB_URI,{
+              dbName:'Problem',
+            })
+        }
     } catch (error) {
       console.log('DB error',error);
       throw error;
--- src/config/server.config.js
+++ src/config/server.config.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 module.exports ={
     PORT : process.env.PORT,
-    MONGODB_URI:process.env.MONGODB_URI
+    MONGODB_URI:process.env.MONGODB_URI,
+    NODE_ENV:process.env.NODE_ENV
 }
