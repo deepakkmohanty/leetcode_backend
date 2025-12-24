1289ade8aeb6552759a4dcf3ede8e82bdbed1386
Deepak Mohanty <mohantydeepak284@gmail.com>
2025-12-24 12:27:33 +0530

    Added logic of sanitized markdown

---
+++ src/utils/markdownSanitizer.js
@@ -0,0 +1,18 @@
+const marked = require('marked');
+const htmlSanitizeLibrary = require('sanitize-html');
+const TurndownService = require('turndown');
+
+function sanitizeMarkdownContent(markdownContent) {
+    const turndownService = new TurndownService();
+    const convertedHtml = marked.parse(markdownContent);
+    console.log(convertedHtml);
+    const sanitizedHtml = htmlSanitizeLibrary(convertedHtml, {
+        allowedTags: htmlSanitizeLibrary.defaults.allowedTags,
+    });
+    console.log(sanitizedHtml);
+
+    const sanitizedMarkdown = turndownService.turndown(sanitizedHtml);
+    return sanitizedMarkdown;
+}
+
+module.exports = sanitizeMarkdownContent;
